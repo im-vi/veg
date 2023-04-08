82752c9c0544f7284ebd56578baa09bbee539aa2
vi <79205177+im-vi@users.noreply.github.com>
2023-04-08 23:48:06 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/games/without-control', function(req, res) {
+    res.sendFile(__dirname + '/withoutcontrol.html')
+})
+
+app.get('/games', function(req, res) {
+    res.sendFile(__dirname + '/games.html')
+})
+
 app.listen('4000', function() {
     console.log('Server is running!')
 })
